c23fbfd7e3d44a8f0b4f2685380e3e3309e0bf12
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-09-23 23:38:31 -0500

    Update url_health_check.js

--- github_actions/url_health_check.js
+++ github_actions/url_health_check.js
@@ -1,12 +1,13 @@
 const https = require('https');
 const { exec } = require('child_process');
-const puppeteer = require('puppeteer'); // Import Puppeteer
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const urlToCheck = process.env.URL_TO_CHECK;
 
 const options = {
   headers: {
-    'User-Agent': 'Mozilla/5.0', // Set a User-Agent header
+    'User-Agent': 'Mozilla/5.0',
   },
 };
 
@@ -14,10 +15,10 @@ function checkHealth(url) {
   return new Promise((resolve, reject) => {
     https.get(url, options, (res) => {
       const statusCode = res.statusCode;
-      const failed = statusCode !== 200 ? 'true' : 'false'; // Set 'true' when failed
+      const failed = statusCode !== 200;
       resolve({ statusCode, failed });
     }).on('error', (err) => {
-      resolve({ statusCode: 0, failed: 'true', error: err.message }); // Set 'true' for errors
+      resolve({ statusCode: 0, failed: true, error: err.message });
     });
   });
 }
@@ -26,7 +27,7 @@ async function captureScreenshot(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({ path: 'screenshot.png' }); // Capture screenshot
+  await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 }
 
@@ -36,23 +37,17 @@ async function run() {
   console.log(`Status Code: ${statusCode}`);
   console.log(`Failed: ${failed}`);
 
-const fs = require('fs');
-
-if (failed) {
+  if (failed) {
     console.error(`Health check failed for URL: ${urlToCheck}`);
     console.error(`Status Code: ${statusCode}`);
 
-    // Capture a screenshot of the URL
     await captureScreenshot(urlToCheck);
     
-    // Convert statusCode to a string and write it to the file
     fs.writeFileSync('STATUS_CODE.txt', statusCode.toString());
-    
-    // Write the failedUrls to the file
     fs.writeFileSync('FAILED_URLS.txt', urlToCheck);
     
-    process.exit(1); // This will cause the script to exit with a non-zero status code
+    process.exit(1);
+  }
 }
 
-}
 run();
